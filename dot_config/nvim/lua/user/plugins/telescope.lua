d9529c1228adaf528c3bf412b6e65b736e45b6a0
Jun-Hong Cheng <qwe854896@gmail.com>
2024-03-24 21:08:43 +0800

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/user/keymaps.lua Add .config/nvim/lua/user/plugins/ibl.lua Update .config/nvim/lua/user/plugins/telescope.lua

--- dot_config/nvim/lua/user/plugins/telescope.lua
+++ dot_config/nvim/lua/user/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = '0.1.6',
 		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function ()
+		config = function()
 			require('telescope').setup {
 				defaults = {
 					file_ignore_patterns = { "node_modules", ".git" },
