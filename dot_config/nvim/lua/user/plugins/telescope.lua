f8d3be781954bc510807cc6964b6fe5f85ad2d06
Jun-Hong Cheng <qwe854896@gmail.com>
2024-03-26 01:23:53 +0800

    Update .config/nvim/lua/user/plugins/telescope.lua

--- dot_config/nvim/lua/user/plugins/telescope.lua
+++ dot_config/nvim/lua/user/plugins/telescope.lua
@@ -28,6 +28,14 @@ return {
 						},
 					},
 				},
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
 			}
 
 			local keyset = vim.keymap.set
@@ -40,6 +48,9 @@ return {
 	{
 		'nvim-telescope/telescope-fzf-native.nvim',
 		build =
-		'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+		'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+		config = function()
+			require('telescope').load_extension('fzf')
+		end,
 	},
 }
