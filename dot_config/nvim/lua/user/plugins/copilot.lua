795f958666faddad28ae476e5a9451fbf84898bf
Jun-Hong Cheng <qwe854896@gmail.com>
2024-03-27 17:08:35 +0000

    config: set copilot filetype to true

--- dot_config/nvim/lua/user/plugins/copilot.lua
+++ dot_config/nvim/lua/user/plugins/copilot.lua
@@ -5,8 +5,23 @@ return {
     event = "InsertEnter",
     config = function()
       require('copilot').setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
+        panel = {
+          enabled = false,
+        },
+        suggestion = {
+          enabled = false,
+        },
+        filetypes = {
+          yaml = true,
+          markdown = true,
+          help = true,
+          gitcommit = true,
+          gitrebase = true,
+          hgcommit = true,
+          svn = true,
+          cvs = true,
+          ["."] = true,
+        },
       }
     end,
   },
